29cc54aadf21cd82bfc9dc532fc8f43b55ffbcab
Paris Dolfman <parisdolfman@gmail.com>
2017-11-18 17:18:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,3 @@
 def valid_move?(board, index)
-board[index] == be_between?(0 , 8) && board[index] == "X" || board[index] == "O"
+board[index] == be_between?(0 , 8) && board[index] != "X" || board[index] != "O"
 end
